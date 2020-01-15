767a53b3bc5aea658678edf57cd9e5e6916f1aef
Celine <github@celinechole.com>
2020-01-15 16:17:47 -0500

    add a route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import ItemContainer from './itemContainer'
+import {Switch, Link, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     Hello
+      <nav className="nav">
+        <Link to="/travel">Navigate to travel</Link>
+      </nav>
+      <Switch>
+        <Route path="/travel" render={() => (<ItemContainer />)} />
+      </Switch>
     </div>
   );
 }
