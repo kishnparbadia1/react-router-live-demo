f2ea744fd0c8e1b949f6b635ca0600e1394540d1
Celine <github@celinechole.com>
2020-01-16 16:00:20 -0500

    add more link

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,9 @@ function Home() {
       <img src="react-router.jpg" alt="react-router logo" />
       <div className="home-links">
         <Link to="/browserrouter">Browswer Router</Link>
-        <Link to="/link">Link and NavLink</Link>
-        <Link to="/switch-route">Switch and Route</Link>
+        <Link to="/link">Link & NavLink</Link>
+        <Link to="/switch-route">Switch & Route</Link>
+        <Link to="/route-props">Route props</Link>
       </div>
     </div>
   );
