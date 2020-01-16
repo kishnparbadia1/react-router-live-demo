afc82474ac6e0dc2e0c8c0383bb8f6b262f6aa1b
Celine <github@celinechole.com>
2020-01-16 16:23:22 -0500

    add resources

--- src/Resources.js
+++ src/Resources.js
@@ -3,13 +3,35 @@ import React from "react";
 function Ressources() {
   return (
     <div className="container">
-      <a
-        href="https://reacttraining.com/react-router/web/guides/quick-start"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        React Training - React Router
-      </a>
+      <h1>Resources</h1>
+      <p>
+        <a
+          href="https://reacttraining.com/react-router/web/guides/quick-start"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          React Training - React Router
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://www.youtube.com/watch?v=Law7wfdg_ls"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          React Router Tutorial | React For Beginners from Dev Ed
+        </a>
+      </p>
+      <p>
+        <a
+          href="https://www.youtube.com/watch?v=QUz3k2O3ZJU"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Complete React Tutorial (& Redux) #25 - The React Router from The Net
+          Ninja
+        </a>
+      </p>
     </div>
   );
 }
