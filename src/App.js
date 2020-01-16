b39a581c554edde23284e5c8718b8dd39e1be5e2
Celine <github@celinechole.com>
2020-01-16 14:27:45 -0500

    add resource

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './Home'
 import BrowserRouter from './BrowserRouter'
 import LinkNavLink from './LinkNavLink';
 import SwitchRoute from './SwitchRoute';
+import Resources from './Resources';
+import RouteProps from './RouteProps'
 
 
 function App() {
@@ -15,11 +17,14 @@ function App() {
         <NavLink to="/browserrouter" activeClassName="nav-selected">Browser Router</NavLink>
         <NavLink to="/link" activeClassName="nav-selected">Link / NavLink</NavLink>
         <NavLink to="/switch-route" activeClassName="nav-selected">Switch / Route</NavLink>
+        <NavLink to="/resources" activeClassName="nav-selected">Resources</NavLink>
       </nav>
       <Switch>
         <Route path="/browserrouter" component={BrowserRouter} />
         <Route path="/link" component={LinkNavLink} />
         <Route path="/switch-route" component={SwitchRoute} />
+        <Route path="/resources" component={Resources} />
+        <Route path="/route-props" component={RouteProps} />
         <Route exact path="/" component={Home} />
       </Switch>
     </div>
---
+++ src/Resources.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Ressources() {
+  return (
+    <div className="container">
+      <a
+        href="https://reacttraining.com/react-router/web/guides/quick-start"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        React Training - React Router
+      </a>
+    </div>
+  );
+}
+
+export default Ressources;
