3e1b8ffa807b5612903b3cd8bbfc601cf4c48417
Celine <github@celinechole.com>
2020-01-30 16:33:02 -0500

    add difference browser router, hash router

--- src/BrowserRouter.js
+++ src/BrowserRouter.js
@@ -12,6 +12,11 @@ function BrowserRouter() {
         </li>
       </ul>
       <img src="/browserrouter.png" alt="screenshot browser router" />
+      <h2>Browser Router or Hash Router?</h2>
+      <ul>
+        <li>Browser Router uses <a href="https://developer.mozilla.org/en-US/docs/Web/API/History_API" target="_blank" rel="noopener noreferrer">history API</a> whichh is unavailable for legacy browsers.</li>
+        <li>Hash Router has no limitation on supporter browser and its use cases are not limited to SPA.</li>
+      </ul>
       <h2>Documentation</h2>
       <a href="https://reacttraining.com/react-router/web/api/BrowserRouter" target="_blank" rel="noopener noreferrer">React training - Browser Router</a>
     </div>
