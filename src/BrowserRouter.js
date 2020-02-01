417ce679bbbad925e07820d5524780e2d25af9cf
CelineChole <github@celinechole.com>
2020-02-01 12:02:55 -0500

    Update BrowserRouter.js

--- src/BrowserRouter.js
+++ src/BrowserRouter.js
@@ -15,7 +15,7 @@ function BrowserRouter() {
       <h2>Browser Router or Hash Router?</h2>
       <ul>
         <li>Browser Router uses <a href="https://developer.mozilla.org/en-US/docs/Web/API/History_API" target="_blank" rel="noopener noreferrer">history API</a> whichh is unavailable for legacy browsers.</li>
-        <li>Hash Router has no limitation on supporter browser and its use cases are not limited to SPA.</li>
+        <li>Hash Router has no limitation on supported browser and its use cases are not limited to SPA.</li>
       </ul>
       <h2>Documentation</h2>
       <a href="https://reacttraining.com/react-router/web/api/BrowserRouter" target="_blank" rel="noopener noreferrer">React training - Browser Router</a>
