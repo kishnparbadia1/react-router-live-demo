8747bedd6105edb3929a44b95b228eb170a0ab10
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2020-08-25 11:22:07 -0400

    Added anchor tags to the home page

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,11 @@ function Home() {
     <div className='container'>
       <img src="react-router.jpg" alt="react-router logo" />
       <div className="home-links">
-        {/*Anchor tags go here*/}
+        <a href="/browser-router">Browser Router</a>
+        <a href="/switch-route">Switch & Route</a>
+        <a href="/link">Link & NavLink</a>
+        <a href="/route-props">Route props</a>
+        <a href='/resources'>Resources</a>
       </div>
     </div>
   );
