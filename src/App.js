6442ae896c13818362b0ad4d22c5ce9db1e6f49f
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-08-25 10:45:52 -0400

    Adding code for 2108 part 1

--- src/App.js
+++ src/App.js
@@ -7,18 +7,30 @@ import SwitchRoute from "./SwitchRoute";
 import Resources from "./Resources";
 import RouteProps from "./RouteProps";
 import FavoriteDessert from "./FavoriteDessert";
+import { NavLink, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <nav className="nav"></nav>
-      <Home />
+      <nav className="nav">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/browser-router" activeClassName="nav-selected">
+          Browser Router
+        </NavLink>
+        <NavLink to="/switch-route">Switch Route</NavLink>
+        <NavLink to="/link-nav-link">Link Nav Link</NavLink>
+      </nav>
+      {/* <Home />
       <BrowserRouter />
-      <SwitchRoute />
+      <SwitchRoute></SwitchRoute>
       <LinkNavLink />
       <Resources />
       <RouteProps />
-      <FavoriteDessert />
+      <FavoriteDessert /> */}
+      <Route path="/" component={Home}></Route>
+      <Route path="/browser-router" component={BrowserRouter}></Route>
+      <Route path="/switch-route" component={SwitchRoute}></Route>
+      <Route path="/link-nav-link" component={LinkNavLink}></Route>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="container">
       <img src="react-router.jpg" alt="react-router logo" />
-      <div className="home-links"></div>
+      <div className="home-links">
+        <Link to="/">Home</Link>
+        <Link to="/browser-router">Browser Router</Link>
+        <Link to="/switch-route">Switch Route</Link>
+        <Link to="/link-nav-link">Link Nav Link</Link>
+      </div>
     </div>
   );
 }
